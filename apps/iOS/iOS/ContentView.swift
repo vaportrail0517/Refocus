226cea2015c5e70cb7829cf1ec7561b4a89611ec
ZawaReck <ozw1504@gmail.com>
2025-12-05 17:11:28 +0900

    　create iOS project

---
+++ apps/iOS/iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS
+//
+//  Created by ReijiOhzawa on 2025/12/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apps/iOS/iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by ReijiOhzawa on 2025/12/05.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
