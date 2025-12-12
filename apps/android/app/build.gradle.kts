b8350987abf79d86fa8a097410d2ad950c793348
schnee <lotus.owl12@gmail.com>
2025-12-12 14:14:45 +0900

    android-v0.4.2-debug

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.refocus"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4       // APK配布時に毎回インクリメント
-        versionName = "0.4.1" // (大きな区切り・互換性のない変更).(後方互換ありの機能追加).(バグ修正など)
+        versionCode = 5       // APK配布時に毎回インクリメント
+        versionName = "0.4.2" // (大きな区切り・互換性のない変更).(後方互換ありの機能追加).(バグ修正など)
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
